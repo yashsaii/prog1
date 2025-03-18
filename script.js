7caf94b9e92af2e7b6ea192412868489582a3ae8
yashsaii <yashsaii7364@gmail.com>
2025-03-18 09:38:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+
